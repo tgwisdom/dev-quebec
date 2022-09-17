6ce3ca7f82f345ae6163b96478a0e70c54711468
Grey <58967769+FlameRender@users.noreply.github.com>
2022-09-17 18:06:29 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,26 +3,56 @@ const path = require('node:path')
 const bodyParser = require('body-parser')
 
 const app = express()
-const {MongoClient} = require('mongodb');
-const client = new MongoClient(uri);
 
-await listDatabases(client);
 
-try {
-  await client.connect();
+//Start of mongo code
+const { MongoClient } = require('mongodb');
 
-  await listDatabases(client);
+async function main() {
+    /**
+     * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+     * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+     */
+    const uri = "mongodb+srv://Inferno:BackRowHeros@cluster0.2vn7fph.mongodb.net/test";
 
-} catch (e) {
-  console.error(e);
-}
+    /**
+     * The Mongo Client you will use to interact with your database
+     * See https://mongodb.github.io/node-mongodb-native/3.6/api/MongoClient.html for more details
+     * In case: '[MONGODB DRIVER] Warning: Current Server Discovery and Monitoring engine is deprecated...'
+     * pass option { useUnifiedTopology: true } to the MongoClient constructor.
+     * const client =  new MongoClient(uri, {useUnifiedTopology: true})
+     */
+    const client = new MongoClient(uri);
 
-finally {
-  await client.close();
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+
+        // Make the appropriate DB calls
+        await listDatabases(client);
+
+    } catch (e) {
+        console.error(e);
+    } finally {
+        // Close the connection to the MongoDB cluster
+        await client.close();
+    }
 }
 
 main().catch(console.error);
 
+/**
+ * Print the names of all available databases
+ * @param {MongoClient} client A MongoClient that is connected to a cluster
+ */
+async function listDatabases(client) {
+    databasesList = await client.db().admin().listDatabases();
+
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+
+// end of mongo code
 
 
 app.use(bodyParser.json())
@@ -30,12 +60,10 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.set('view engine', 'ejs');
 
 
-let userName = "Joelzzzzz"; 
+let userName = "UnKnoWn"; 
 let thatData = ""; 
 
-async function main() {
-	// we'll add code here soon
-}
+
 
 app.get('/', function (req, res) {
     // res.sendFile(path.join(__dirname, "index.html" )); 
